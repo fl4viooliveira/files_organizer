d772c0f35e7a0453c7de4489a4361db9e7488ade
fl4viooliveira <fl4viooliveira@gmail.com>
2020-06-19 22:39:34 +0100

    testing the methods

---
+++ organizer.py
@@ -0,0 +1,11 @@
+
+
+
+
+# todo - open folders tree
+
+
+# todo - classify folders
+
+
+# todo - create folders to organize files
--- test_os.py
+++ test_os.py
@@ -1,5 +1,38 @@
+# todo - test functionalities
 
 import os
+from pathlib import Path
+
+cur_dir = Path.cwd()
+print(cur_dir)
+
+path = '/home/flavio/Projects/FilesOrganizer/test_folder'
+path_empty = '/home/flavio/Projects/FilesOrganizer/empty'
+
+# This method is used to check whether the given path points to an existing file or directory or not.
+obj = Path(path)
+print(obj.exists())
+
+# This method is used to check whether the given path is a directory or not.
+obj2 = Path(path_empty)
+print(obj2.is_dir())
+
+# the directory empty or not
+dir = os.listdir(path)
+dir2 = os.listdir(path_empty)
+
+if len(dir) == 0:
+    print("Empty directory")
+else:
+    print("Not empyt directory")
+
+if len(dir2) == 0:
+    print("Empty directory")
+else:
+    print("Not empyt directory")
+
+
+
 print(os.name)
 print(os.getcwd())
 
