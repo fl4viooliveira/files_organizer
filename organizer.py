5ad8ee89cde4b87cab4e2a43365961951f8868e8
fl4viooliveira <fl4viooliveira@gmail.com>
2020-06-22 13:38:17 +0100

    (testing) os.scandir and os.walk

--- organizer.py
+++ organizer.py
@@ -1,12 +1,23 @@
 
 import os
+from pathlib import Path
 
-folder_path = "/home/flavio/Projects/FilesOrganizer/test_folder"
+folder_path = "/home/flavio/Projects/FilesOrganizer/test_folder/"
 included_extentions = ['jpg','jpeg', 'bmp', 'png', 'gif']
-file_names = [fn for fn in os.listdir(folder_path)
-              if any(fn.endswith(ext) for ext in included_extentions)]
 
-print(file_names)
+with os.scandir(folder_path) as it:
+    for entry in it:
+        if not entry.name.startswith('.') and entry.is_file():
+            print(entry.name)
+
+for root, subdirs, files in os.walk(folder_path):
+    print(root, subdirs)
+
+# files_names = [fn for fn in os.listdir(folder_path)
+#               if any(fn.endswith(ext) for ext in included_extentions)]
+#
+# print(files_names)
+
 
 
 
