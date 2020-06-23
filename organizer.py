c32e8cacc87407eeb9bdb85594673b760fc5fdb4
fl4viooliveira <fl4viooliveira@gmail.com>
2020-06-23 10:43:35 +0100

    endswith as a tuple

--- organizer.py
+++ organizer.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 folder_path = "/home/flavio/Projects/FilesOrganizer/test_folder/"
 included_extentions = ['.jpg', '.jpeg', '.bmp', '.png', '.gif']
-
+cwd = os.getcwd()
 
 for root, dirs, files in os.walk(folder_path):
     for file in files:
-        if file.endswith('.jpg'):
+        if file.endswith(tuple(included_extentions)):
             print(os.path.join(root, file))
 
 
