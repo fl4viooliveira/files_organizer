65a81651e05accaa5632ef2e875417e7ecea1bb8
fl4viooliveira <fl4viooliveira@gmail.com>
2020-06-28 18:02:16 +0100

    working on interface

---
+++ interface.py
@@ -0,0 +1,37 @@
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+
+
+class Root (Tk):
+    def __init__(self):
+        super(Root, self).__init__()
+        self.title("Tkinter Dialog Widget")
+        self.minsize(640, 400)
+        self.wm_iconbitmap()
+        # self.configure(background = '#4D4D4D')
+
+        self.labelFrame = ttk.LabelFrame(self, text="Open a Folder")
+        self.labelFrame.grid(column=0, row=1, padx=20, pady=20)
+
+        self.button()
+
+
+    def button(self):
+        self.button = ttk.Button(self.labelFrame, text="Browse a Folder", command=self.folderDialog)
+        self.button.grid(column=1, row=1)
+
+
+    def folderDialog(self):
+        self.foldername = filedialog.askdirectory()
+        self.label=ttk.Label(self.labelFrame, text="")
+        self.label.grid(column=1, row=2)
+        self.label.configure(text=self.foldername)
+
+        print(str(self.foldername))
+
+
+if __name__ == '__main__':
+    root = Root()
+    root.mainloop()
